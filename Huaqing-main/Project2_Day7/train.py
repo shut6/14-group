3d23fcb27ac735f98f23a07af0288b7ab54cc71b
shut6 <xxzzaasss123@163.com>
2025-06-21 16:49:40 +0800

    Delete Huaqing-main directory

--- Huaqing-main/Project2_Day7/train.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-if __name__ == '__main__':
-    model = YOLO("yolov8n.yaml").load("yolov8n.pt")
-    model.train(data = r"E:\PythonLab\Huaqing_Ruiyang\pythonProject\Project2_Day5\ultralytics-main\TrafficSignDetection.v9\TrafficSignDetection.v9.yaml",imgsz=640,epochs=20,batch=4)
